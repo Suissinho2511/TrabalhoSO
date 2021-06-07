10a13db9cd54a8ef66c3ecc9ccc110915e50bd5e
Diogo Matos <di@pop-os.localdomain>
2021-06-07 18:57:18 +0100

    alpha1-0

--- src/aurras.c
+++ src/aurras.c
@@ -4,18 +4,55 @@
 #include <stdio.h>
 #include <fcntl.h>
 #include <string.h>
+#include <signal.h>
+#include <stdlib.h>
+
+#define BUFFER_SIZE 1024
+#define QUEUE_NAME "queue"
+
+int status;
+
+void progress_signal(int signum)
+{
+	status++;
+	if (status == 1) {printf("processing "); pause();}
+	if (status == 2) {printf("finished"); alarm(1);}
+}
+void error_signal(int signum)
+{
+	printf("Signal %d received. Server error.", signum);
+}
 
 int main(int argc, char **argv)
 {
-	int pipe_fd = open("tmp", O_WRONLY);
+	signal(SIGUSR1, progress_signal);
+	signal(SIGUSR2, error_signal);
+	signal(SIGALRM, SIG_IGN);
+
+	char stdout_buffer[BUFFER_SIZE];
+	setvbuf(stdout, stdout_buffer, _IONBF, BUFFER_SIZE);
+
+	int queue_fd = open(QUEUE_NAME, O_WRONLY);
 	printf("Session open (pid: %d)!", getpid());
 
-	int bytes_read = 0;
+	//argv[1] ficheiro original
+	//argv[2] ficheiro destino
+	//argv[3-] filtros
+
+	int size = 0;
 	char buffer[1024];
 
-	bytes_read = read(0, buffer, 1024);
-	write(pipe_fd, buffer, bytes_read);
+	size += sprintf(buffer, "%d ", getpid());
+	for ( int i = 1; i < argc; i++)
+	{
+		size += sprintf(&buffer[size],"%s ", argv[i]);
+	}
+	buffer[size] = '\n';
+	write(queue_fd, buffer, size+1);
 
-	close(pipe_fd);
+	printf("pending ");
+	pause();
+
+	close(queue_fd);
 	return 0;
 }
--- src/aurrasd.c
+++ src/aurrasd.c
@@ -3,26 +3,116 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <fcntl.h>
+#include <string.h>
+#include <signal.h>
+#include <stdlib.h>
+
+#define BUFFER_SIZE 1024
+#define QUEUE_NAME "queue"
+
+int readLn(int fd, char* buffer, int size);
+char** parse(char* buffer, int size, char* delim);
+void freearr(void** pointer, int size);
+int myexec(int in_fd, int out_fd, char* bin_name, char** args);
+
+void sigterm_handler(int signum)
+{
+	printf("Closing server...\n");
+	//...
+	unlink(QUEUE_NAME);
+	printf("Server closed!\n");
+}
 
 int main(int argc, char **argv)
 {
-	mkfifo("tmp", 0666);
-	int pipe_fd = open("tmp", O_RDONLY);
+	signal(SIGTERM, sigterm_handler);
+
+	char stdout_buffer[BUFFER_SIZE];
+	setvbuf(stdout, stdout_buffer, _IOLBF, BUFFER_SIZE);
+
+	mkfifo(QUEUE_NAME, 0666);
+	int queue_fd = open(QUEUE_NAME, O_RDONLY);
 	printf("Server Online (pid: %d)!\n", getpid());
 
-	int bytes_read = 0;
-	char buffer[1024];
+	int bytes_read = 0, pid;
+	char buffer[BUFFER_SIZE];
+	char** parsed;
 	while(1)
 	{
-		printf("Waiting for request...\n");
-		while((bytes_read = read(pipe_fd, buffer, 1024)) > 0)
+		printf("Waiting for request... ");
+		fflush(stdout);
+		while((bytes_read = readLn(queue_fd, buffer, BUFFER_SIZE)) > 0)
 		{
-			printf("Request received.");
+			//queue:
+			//pid filenameoriginal filenamedestino filtro0 filtro1 ...\n
+			parsed = parse(buffer, bytes_read, " ");
+			printf("Request received (pid: %s).\n", parsed[0]);
+
+			//pai -> Controller -> filho(ffmpeg)
+			if((pid = fork()) == 0)
+			{
+				pid = myexec(stdin, stdout, "ffmpeg", &parsed[1]);
+				kill(atol(parsed[0]), SIGUSR1); //processing
+				wait(pid);
+				kill(atol(parsed[0]), SIGUSR1); //finished
+				_exit(0);
+			}
+			freearr(parsed, 0); //wrong btw
+		}
+	}
+	return 0;
+}
+
+int readLn(int fd, char* buffer, int size)
+{
+	int bytes_read = 0;
+	while((bytes_read += read(fd, buffer+bytes_read, 1)) > 0 && bytes_read < size)
+	{
+		if(buffer[bytes_read] == '\n') break;
+	}
+	buffer[bytes_read] = NULL;
+
+	return bytes_read;
+}
+
+char** parse(char* buffer, int size, char* delim)
+{
+	char* token;
+	char** parsed = calloc(size+1, sizeof(char));
+
+	token = strtok(buffer, delim);
+	for(int i = 0; ; i++)
+	{
+		parsed[i] = strdup(token);
+		token = strtok(NULL, delim);
+		if(!token)
+		{
+			parsed[i+1] = NULL;
+			break;
 		}
 	}
 
-	close(pipe_fd);
-	unlink("tmp");
-	printf("Server closed!");
-	return 0;
+	return parsed;
 }
+
+void freearr(void** pointer, int size)
+{
+	for(int i = 0; i < size; i++) free(pointer[i]);
+	free(pointer);
+	return;
+}
+
+int myexec(int in_fd, int out_fd, char* bin_name, char** args)
+{
+	int pid;
+	if((pid = fork()) == 0)
+	{
+		//filho
+		dup2(in_fd, stdin);
+		dup2(out_fd, stdout);
+		execvp(bin_name, args); //wrong ?
+	}
+	else return pid;
+}
+
+
