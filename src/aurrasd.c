fc6e722231932c77d1a9721d5a74433abbb8e8c6
Diogo Matos <di@pop-os.localdomain>
2021-06-10 19:33:43 +0100

    beta0.0

--- src/aurrasd.c
+++ src/aurrasd.c
@@ -9,8 +9,8 @@
 #include <wait.h>
 
 #define BUFFER_SIZE 1024
-#define QUEUE_NAME "queue"
-#define STATUS_NAME "status"
+#define QUEUE_NAME "../tmp/queue"
+#define STATUS_NAME "../tmp/status"
 
 int readLn(int fd, char* buffer, int size);
 int parse(char** parsed, char* buffer, int size, char* delim);
@@ -18,13 +18,14 @@ void freearr(void** pointer, int size);
 int myexec(int in_fd, int out_fd, char* bin_name, char** args);
 
 typedef struct status {
-	int pid_server;
-	char* filtros;
-	char* filtros_traduzidos;
-	int* max;
-	int* running;
-	char** tasks;
+	int pid_server, num_filters;
+	int *max, *running;
+	char **filters, **filtersT, **tasks;
 } *STATUS;
+
+STATUS newStatus();
+STATUS readStatus(STATUS s, char* conf_filepath);
+
 
 void sigterm_handler(int signum)
 {
@@ -36,30 +37,36 @@ void sigterm_handler(int signum)
 
 int main(int argc, char **argv)
 {
-	signal(SIGTERM, sigterm_handler);
+	signal(SIGTERM, sigterm_handler);				//Handling sigterm to slowly close server
 
 	char stdout_buffer[BUFFER_SIZE];
-	setvbuf(stdout, stdout_buffer, _IOLBF, BUFFER_SIZE);
+	setvbuf(stdout, stdout_buffer, _IOLBF, BUFFER_SIZE);		//stdout to Line Buffered
 
-	mkfifo(QUEUE_NAME, 0666);
-	int queue_fd = open(QUEUE_NAME, O_RDONLY);
-	int status_fd = open(STATUS_NAME, O_RDWR | O_CREAT | O_TRUNC);
-	int conf_fd = open(argv[1], O_RDONLY | O_EXCL);
+	mkfifo(QUEUE_NAME, 0666);					//create fifo queue
+	int queue_fd = open(QUEUE_NAME, O_RDONLY);			//open fifo queue
+	int status_fd = open(STATUS_NAME, O_RDWR | O_CREAT | O_TRUNC);	//create status
+
+	STATUS s = newStatus();
+	s = readStatus(s, argv[1]);					//ler o status
+
 	printf("Server Online (pid: %d)!\n", getpid());
-	//TODO: Interpretar Config file
 
-	int bytes_read = 0, size, pid, pid_cliente, wstatus;
+	int bytes_read = 0, size, pid, pid_cliente, wstatus, task_num = 0;
 	char buffer[BUFFER_SIZE];
-	char** parsed = calloc(1, sizeof(char*)); //temporário
+	char** parsed = calloc(s->num_filters+4, sizeof(char*));
 	while(1)
 	{
 		printf("Waiting for request... ");
 		fflush(stdout);
 		while((bytes_read = readLn(queue_fd, buffer, BUFFER_SIZE)) > 0)
 		{
+			//to be improved:
+			s -> tasks[task_num] = strdup(buffer);
+			task_num++;
+
 			//queue:
-			//pid filenameoriginal filenamedestino filtro0 filtro1 ...\n
-			size = parse(parsed, buffer, bytes_read, " ");
+			//pid transform filenameoriginal filenamedestino filtro0 filtro1 ...\n
+			size = parse(parsed, buffer, s->num_filters, " ");
 			printf("\rRequest received (pid: %s).\n", parsed[0]);
 
 			//TODO: Limitar o numero de processos ativos simultaneamente
@@ -67,7 +74,8 @@ int main(int argc, char **argv)
 			//server -> Controller -> filho(ffmpeg)
 			if((pid = fork()) == 0)
 			{
-				pid_cliente = atol(parsed[0]);	
+				pid_cliente = atol(parsed[0]);
+				//TODO: encontrar o indice do filtro. Não sei como aplicar o filtro
 				pid = myexec(0, 1, "ffmpeg", parsed); //replaces parsed[0] with bin_name btw
 
 				kill(pid_cliente, SIGUSR1); 		//processing
@@ -79,9 +87,8 @@ int main(int argc, char **argv)
 
 				_exit(0);				//exits
 			}
-			freearr((void**) parsed, size); //not gonna stay here
 		}
-		freearr((void**) parsed, 1); //temporário
+		memset(parsed, 0, s->num_filters * sizeof(char*)); //a lot of memory leaks xd
 	}
 	return 0;
 }
@@ -100,7 +107,6 @@ int readLn(int fd, char* buffer, int size)
 
 int parse(char** parsed, char* buffer, int size, char* delim)
 {
-	//char** parsed = calloc(size+1, sizeof(char));
 	int i;
 	char* token = strtok(buffer, delim);
 	for(i = 0; ; i++)
@@ -138,4 +144,34 @@ int myexec(int in_fd, int out_fd, char* bin_name, char** args)
 	return pid;
 }
 
+STATUS newStatus()
+{
+	STATUS r = calloc(1, sizeof(struct status));
+	r -> pid_server = getpid();
+	//TODO: alocar espaço para os arrays
+	return r;
+}
+
+STATUS readStatus(STATUS s, char* conf_filepath)
+{
+	int conf_fd = open(conf_filepath, O_RDONLY | O_EXCL);	//open conf
+
+	int bytes_read = 0;
+	char buffer[BUFFER_SIZE], parsed[BUFFER_SIZE][BUFFER_SIZE];
+	for(int i = 0; bytes_read > 0; i++)
+	{
+		bytes_read = readLn(conf_fd, buffer, BUFFER_SIZE);
+		s -> num_filters ++;
+
+		//conf:
+		//filtro filtrotraduzido max
+		parse(parsed, buffer, 1024, " ");
+		s -> filters[i] = strdup(parsed[0]);
+		s -> filtersT[i] = strdup(parsed[1]);
+		s -> max[i] = atol(parsed[2]);
+
+	}
+
+	return s;
+}
 
